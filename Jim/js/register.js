eef9038fccc02eadb878efdb64dedd3a578e799a
Dan Bal <db3260@columbia.edu>
2019-03-11 23:10:35 +0000

    started register

---
+++ Jim/js/register.js
@@ -0,0 +1,8 @@
+function register() {
+    var register_email = document.getElementById("register_email").value;
+    var register_username = document.getElementById("register_username").value;
+    var register_password = document.getElementById("register_password").value;
+    var confirm_password = document.getElementById("conf_pass").value;
+
+
+}
