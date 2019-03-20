c39b889a76639e1f05092c79fa035211547ad8a8
Dan Bal <db3260@columbia.edu>
2019-03-20 22:34:43 +0000

    began validation

--- Jim/js/register.js
+++ Jim/js/register.js
@@ -1,8 +1,42 @@
-function register() {
-    var register_email = document.getElementById("register_email").value;
-    var register_username = document.getElementById("register_username").value;
-    var register_password = document.getElementById("register_password").value;
-    var confirm_password = document.getElementById("conf_pass").value;
+// Input fields
+const username_field = document.getElementById("register_username");
+const email_field = document.getElementById("register_email");
+const password_field = document.getElementById("register_password");
+const conf_pass_field = document.getElementById("conf_pass");
+const submit_btn = document.getElementById("btn-register");
+
+// Invalid colour
+const red = '#ff0000';
 
 
+function validateUsername() {
+    // Checks if username input empty
+    checkIfEmpty(username_field);
+}
+
+function checkIfEmpty(field) {
+    // Trims white spaces
+    if (isEmpty(field.value.trim)) {
+        // Field set invalid
+        setInvalid(field, `${field.name} cannot be empty`);
+        return true
+    } else {
+        // Field set valid
+        setValid(field);
+        return false
+    }
+}
+
+function isEmpty(value) {
+    // Returns true if value = whitespace, false if false
+    return value === '';
+}
+function setInvalid(field, message) {
+    // field.className = "invalid";
+    field.nextElementSibling.innerHTML = message;
+    field.nextElementSibling.style.color = red;
+}
+function setValid(field) {
+    // field.className = "valid";
+    field.nextElementSibling.innerHTML = '';
 }
