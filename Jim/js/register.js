8864b84bb206045aa580f3a284e081417fac7b46
Dan Bal <db3260@columbia.edu>
2019-03-22 22:21:26 +0000

    validation: checks if fields are empty

--- Jim/js/register.js
+++ Jim/js/register.js
@@ -1,21 +1,38 @@
 // Input fields
+// Register
 const username_field = document.getElementById("register_username");
 const email_field = document.getElementById("register_email");
 const password_field = document.getElementById("register_password");
 const conf_pass_field = document.getElementById("conf_pass");
 const submit_btn = document.getElementById("btn-register");
 
+// Login
+const email_login = document.getElementById("login_email");
+const password_login = document.getElementById("login_password");
+
 // Invalid colour
 const red = '#ff0000';
 
 
-function validateUsername() {
-    // Checks if username input empty
+function validateInputsRegister() {
+    // Checks if register inputs input empty
     checkIfEmpty(username_field);
     checkIfEmpty(email_field);
+}
+
+function validateInputsLogin() {
+    // Checks if login inputs empty
+    checkIfEmpty(email_login);
+    checkIfEmpty(password_login);
+}
+
+function validatePassword() {
+    // Checks if password field is empty
     checkIfEmpty(password_field);
-    checkIfEmpty(conf_pass_field);
-    checkPasswordsMatch()
+    // Checks if password meets length requirements
+    !meetLength(password_field, 6, 100);
+    // Create password security requirements e.g. at least 1 number
+    !containCharacters(password_field, 1);
 }
 
 function checkIfEmpty(field) {
@@ -49,5 +66,40 @@ function setValid(field) {
     $ (document).ready(function () {
         $(field).css('border-bottom', '1px solid white')
     });
+}
+
+function meetLength(field, minLength, maxLength) {
+    if (field.value.length >= minLength && field.value.length < maxLength) {
+        setValid(field);
+        return true;
+    } else if (field.value.length < minLength) {
+        setInvalid(field, `${field.name} must be at least ${minLength} characters`);
+        return false;
+    } else {
+        setInvalid(field, `${field.name} must be shorter than ${maxLength} characters`);
+        return false;
+    }
+}
+
+function containCharacters(field, code) {
+    let regEx;
+    switch (code) {
+        case 1:
+            // Letters
+            regEx = /(?=.*[a-zA-Z])/;
+            return matchWithRegEx(regEx, field, `Must contain at least 1 letter`);
+        default:
+            return false;
+    }
+}
+
+function matchWithRegEx(regEx, field, message) {
+    if (field.value.match(regEx)){
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field, message);
+        return false;
+    }
 }
 
